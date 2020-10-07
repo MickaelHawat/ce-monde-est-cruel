9ef9de7a9c389603cf2fcd4f7bba32ec32a92eac
MickaelHawat <61239654+MickaelHawat@users.noreply.github.com>
2020-10-07 21:51:59 +0200

    Add files via upload

--- src/Game/PlayerIA/RuusanPlayer.php
+++ src/Game/PlayerIA/RuusanPlayer.php
@@ -7,7 +7,10 @@ use Hackathon\Game\Result;
 /**
  * Class RuusanPlayers
  * @package Hackathon\PlayerIA
- * @author EL HAWAT Mickael
+ * @author Mickael EL HAWAT
+ * Je voulais au début créer un sorte de random grace au décimal de pi. 
+ * Cependant après avoir vu que cette technique était beaucoup trop complexe et pas satisfaisante. 
+ * Je suis partit sur une technique plus simple qui fait que je ne joue jamais 2 fois mon coup et prend le coup le plus fort entre les 2 restants
  */
 class RuusanPlayer extends Player
 {
@@ -15,58 +18,25 @@ class RuusanPlayer extends Player
     protected $opponentSide;
     protected $result;
 
-    public $a = 22;
-    public $b = 7;
-    public $n = 1;
-    public $f = 1;
 
     public function getChoice()
     {
-        $c = ($this->a / $this->b);
-        $k = $this->result->getNbRound();
-        if($k == 0)
+
+        if ($this->result->getLastChoiceFor($this->mySide) == parent::rockChoice())
         {
-            $this->n = $this->n*10;
-            $this->f = $this->f + 1;
-            return parent::paperChoice();
-        }
-        if($this->f > 10)
-        {
-            $this->a = $this->a + 3;
-            $c = $this->a / $this->b;
-            $this->f = 1;
-            $this->n = 1;
-        }
-        if (($c*$this->n)%10 > 6)
-        {
-            $this->n = $this->n*10;
-            $this->f = $this->f + 1;
-            if ($this->result->getLastChoiceFor($this->mySide) == parent::rockChoice())
-            {
-                return parent::scissorsChoice();
-            }
-            return parent::rockChoice();
-        }
-        elseif (($c*$this->n)%10 > 3)
-        {
-            $this->n = $this->n*10;
-            $this->f = $this->f + 1;
-            if ($this->result->getLastChoiceFor($this->mySide) == parent::paperChoice())
-            {
-                return parent::rockChoice();
-            }
-            return parent::paperChoice();
-        }
-        else
-        {
-            $this->n = $this->n*10;
-            $this->f = $this->f + 1;
-            if ($this->result->getLastChoiceFor($this->mySide) == parent::scissorsChoice())
-            {
-                return parent::paperChoice();
-            }
             return parent::scissorsChoice();
         }
 
+        if ($this->result->getLastChoiceFor($this->mySide) == parent::paperChoice())
+        {
+            return parent::rockChoice();
+        }
+
+        if ($this->result->getLastChoiceFor($this->mySide) == parent::scissorsChoice())
+        {
+            return parent::paperChoice();
+        }
+
+        return parent::paperChoice();
     }
 };
